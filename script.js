407ed16b756d082b6e415f6d366e4a6960080470
B3an5 <beantheo@gmail.com>
2018-09-03 13:08:57 -0600

    constructor function and set local storage

--- script.js
+++ script.js
@@ -10,16 +10,9 @@ $(saveButton).on('click', createIdea)
 
 function createIdea(event) {
   event.preventDefault();
+  var newCard = new Card(ideaTitle.value, ideaBody.value);
   var idea = document.createElement('div'); 
-  idea.innerHTML = 
-            `<h2>${ideaTitle.value}</h2> 
-            <button class="delete-button"></button>
-            <p class="idea-details">${ideaBody.value}</p>
-            <p class="idea-quality">
-            <button class="upvote-button"></button>
-            <button class="downvote-button"></button> 
-            quality: <span class="quality-value">swill</span></p>
-            <hr>`
+  createHTML(idea);
   document.getElementById('output-section').prepend(idea);
   var deleteButton = document.querySelector('.delete-button');
   var upvoteButton = document.querySelector('.upvote-button');
@@ -29,6 +22,19 @@ function createIdea(event) {
   downVoteButton.addEventListener('click', downVote);
   ideaTitle.value = '';
   ideaBody.value = '';
+  storeIdea(newCard);
+}
+
+function createHTML(idea) {
+  idea.innerHTML = 
+            `<h2>${ideaTitle.value}</h2> 
+            <button class="delete-button"></button>
+            <p class="idea-details">${ideaBody.value}</p>
+            <p class="idea-quality">
+            <button class="upvote-button"></button>
+            <button class="downvote-button"></button> 
+            quality: <span class="quality-value">swill</span></p>
+            <hr>`
 }
   
 function deleteIdea(event) {
@@ -56,6 +62,18 @@ function downVote(event) {
   }
  }
 };
+
+function storeIdea(poop) {
+  var stringifiedIdea = JSON.stringify(poop);
+  localStorage.setItem('rememberMe', stringifiedIdea);
+};
+
+function Card(title, body) {
+  this.title = title;
+  this.body = body;
+  this.identifier = Date.now();
+  this.quality = 'swill';
+}
 
 
 
